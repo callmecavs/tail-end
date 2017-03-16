fb692e9748ba2a7925c6d16994c917ede8bc42fa
callmecavs <callmecavs@gmail.com>
2017-03-15 22:02:39 -0400

    some setup, and commenting

--- src/tail-end.js
+++ src/tail-end.js
@@ -1,5 +1,25 @@
-const tailEnd = () => {
+const toString = Object.prototype.toString
 
-}
+const type = thing => toString
+  .call(thing)
+  .slice(8, -1)
+
+const tailEnd = (config, style) => new Promise((resolve, reject) => {
+  // create the transitionend handler
+  const handler = event => {
+    // TODO: check the property name(s)
+    // TODO: unbind the handler
+    // TODO: resolve, do we want to pass anything on?
+  }
+
+  // TODO: bind the transitionend handler
+
+  // check for no style, meaning an early exit
+  if (!style) {
+    return
+  }
+
+  // TODO: apply string of style, or apply object to element.style
+})
 
 export default tailEnd
